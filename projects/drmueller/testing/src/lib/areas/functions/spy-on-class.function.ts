a2cf8e16e99b74d14d480cbc28069bfa7af292cf
Matthias Mueller <matthiasm@live.de>
2018-08-31 11:18:37 +0200

    WIP

--- projects/drmueller/testing/src/lib/areas/functions/spy-on-class.function.ts
+++ projects/drmueller/testing/src/lib/areas/functions/spy-on-class.function.ts
@@ -1,3 +1,5 @@
+/// <reference path="../../../../../../../node_modules/@types/jasmine/index.d.ts" />
+/// <reference path="../../../../../../../node_modules/@types/jasminewd2/index.d.ts" />
 import { Type } from '@angular/core';
 import { SpyOf } from '../types/spy-of.type';
 
