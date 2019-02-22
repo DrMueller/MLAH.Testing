e732c4d886c56d1f7197c085464c897545660dd5
Matthias Mueller <matthiasm@live.de>
2019-02-22 12:22:44 +0100

    WIP

--- projects/drmueller/testing/src/lib/areas/jasmine-extensions/factories/spy-factory.service.ts
+++ projects/drmueller/testing/src/lib/areas/jasmine-extensions/factories/spy-factory.service.ts
@@ -1,27 +1,28 @@
 import { Type } from '@angular/core';
 
+import { ObjectProperty } from '../models';
 import { SpyOf } from '../types';
 
 export class SpyFactoryService {
   public static createSpy<T>(spiedClass: Type<T>): SpyOf<T> {
-    const functionNames = this.getFunctionNames(spiedClass).map(f => f.propName);
+    const functionNames = this.getFunctionNames(spiedClass).map(f => f.propertyName);
 
     // Add the functions
     const result = jasmine.createSpyObj('spy', [...functionNames]);
 
-    // Add "empty" setters per setter property of the spied Class
+    // Add "empty" setters per setter-property of the spied Class
     this.getSetters(spiedClass)
       .map(prop => {
-        Object.defineProperty(result, prop.propName, {
+        Object.defineProperty(result, prop.propertyName, {
           set: function () {
           }
         });
       });
 
-    // Add getters, which return undefined, for each getter property of the spied Class
+    // Add getters, which return undefined, for each getter-property of the spied Class
     this.getGetters(spiedClass)
       .map(prop => {
-        Object.defineProperty(result, prop.propName, {
+        Object.defineProperty(result, prop.propertyName, {
           get: function () {
             return undefined;
           }
@@ -31,48 +32,25 @@ export class SpyFactoryService {
     return result;
   }
 
-  private static getFunctionNames<T>(spiedClass: Type<T>): { propName: string, propDescr: PropertyDescriptor }[] {
+  private static getFunctionNames<T>(spiedClass: Type<T>): ObjectProperty[] {
     return this.getObjects(spiedClass)
-      .filter(prop => prop.propDescr.value instanceof Function);
+      .filter(prop => prop.propertyDescriptor.value instanceof Function);
   }
 
-  private static getGetters<T>(spiedClass: Type<T>): { propName: string, propDescr: PropertyDescriptor }[] {
-    const result = this.getObjects(spiedClass)
-      .filter(prop => prop.propDescr.get)
-      .map(prop => {
-        return {
-          propName: prop.propName,
-          propDescr: prop.propDescr,
-        };
-      });
-
-    return result;
+  private static getGetters<T>(spiedClass: Type<T>): ObjectProperty[] {
+    return this.getObjects(spiedClass).filter(prop => prop.propertyDescriptor.get);
   }
 
-  private static getObjects<T>(spiedClass: Type<T>): { propName: string, propDescr: PropertyDescriptor }[] {
+  private static getObjects<T>(spiedClass: Type<T>): ObjectProperty[] {
     const prototype = spiedClass.prototype;
     const result = Object.getOwnPropertyNames(prototype)
       .filter(name => !!Object.getOwnPropertyDescriptor(prototype, name))
-      .map(name => {
-        return {
-          propName: name,
-          propDescr: Object.getOwnPropertyDescriptor(prototype, name)!
-        };
-      });
+      .map(name => new ObjectProperty(name, Object.getOwnPropertyDescriptor(prototype, name)!));
 
     return result;
   }
 
-  private static getSetters<T>(spiedClass: Type<T>): { propName: string, propDescr: PropertyDescriptor }[] {
-    const result = this.getObjects(spiedClass)
-      .filter(prop => prop.propDescr.set)
-      .map(prop => {
-        return {
-          propName: prop.propName,
-          propDescr: prop.propDescr,
-        };
-      });
-
-    return result;
+  private static getSetters<T>(spiedClass: Type<T>): ObjectProperty[] {
+    return this.getObjects(spiedClass).filter(prop => prop.propertyDescriptor.set);
   }
 }
